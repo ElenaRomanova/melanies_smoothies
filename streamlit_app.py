745b4a6ce22ddbef926fb603e0344eeea65ccca6
Elena Romanova <ElenaRomanova@users.noreply.github.com>
2024-09-10 22:02:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,4 @@ if ingredients_list:
         st.success(name_on_order + ', your Smoothie is ordered!', icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
